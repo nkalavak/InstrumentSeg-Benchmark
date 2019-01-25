d365dfe0b37fa0af2da832aedb86437d2ecc837d
Niveditha Kalavakonda Chandrasekar <nivedithak@nvidia.com>
2019-01-25 10:00:47 -0800

    Added eval metrics

---
+++ evaluation_metrics.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+
+from random import randint
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+import seaborn as sns
+sns.set_style("white")
+
+from sklearn.model_selection import train_test_split
+
+from skimage.transform import resize
+
+from keras.preprocessing.image import load_img
+from keras import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.utils.vis_utils import plot_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Dropout,BatchNormalization
+from keras.layers import Conv2D, Concatenate, MaxPooling2D
+from keras.layers import UpSampling2D, Dropout, BatchNormalization
+from tqdm import tqdm_notebook
+from keras.losses import binary_crossentropy
+from keras import backend as K
+import tensorflow as tf
+K.set_session
+
+#Params and helpers
+img_size_ori = 101
+img_size_target = 512
+
+def upsample(img):
+    if img_size_ori == img_size_target:
+        return img
+    return resize(img, (img_size_target, img_size_target), mode='constant', preserve_range=True)
+    #res = np.zeros((img_size_target, img_size_target), dtype=img.dtype)
+    #res[:img_size_ori, :img_size_ori] = img
+    #return res
+    
+def downsample(img):
+    if img_size_ori == img_size_target:
+        return img
+    return resize(img, (img_size_ori, img_size_ori), mode='constant', preserve_range=True)
+    #return img[:img_size_ori, :img_size_ori]
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+    loss = weight * (logit_y_pred * (1. - y_true) + 
+                     K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+def weighted_dice_loss(y_true, y_pred, weight):
+    smooth = 1.
+    w, m1, m2 = weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    loss = 1. - K.sum(score)
+    return loss
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd
+    averaged_mask = K.pool2d(
+            y_true, pool_size=(50, 50), strides=(1, 1), padding='same', pool_mode='avg')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight = 5. * K.exp(-5. * K.abs(averaged_mask - 0.5))
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + dice_loss(y_true, y_pred)
+    return loss
+
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
